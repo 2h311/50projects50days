22c32238b5ce8355e6271724cec259b06675422e
2h311 <kcodepal@outlook.com>
2022-09-06 15:23:18 +0100

    I'm currently having some issues with the JS. Although i'm almost through with it, just ran into an infinite loop just now

--- todo-ist/index.js
+++ todo-ist/index.js
@@ -3,56 +3,13 @@ let formInput = document.querySelector(".form-input");
 let todoList = document.querySelector("ul.todo-list");
 
 let localTodos = JSON.parse(localStorage.getItem("todos"));
+console.log(localTodos);
 
-if (!localTodos){
-  // create a default value if it dont exist in the storage
-  localTodos = [];
-  localStorage.setItem("todos", JSON.stringify(localTodos)) 
+if (localTodos){
+  // add todos to UI
 }
 
-form.addEventListener('submit', function(event){
-  event.preventDefault();
 
-  // get the text the user input 
-  formInputValue = formInput.value;
-  // set the input value back to empty
-  if (formInputValue !== '') {
-    formInput.value = '';
-    addTodoItem(formInputValue)
-  }
-})
-
-const addTodoItem = (todoText) =>{
-  // create a list item to add the todo-item into
-  let todoItem = document.createElement("li");
-  todoItem.classList.add('todo-item');
-  todoItem.innerHTML = todoText;
-  todoList.append(todoItem);
+function addTodoToUI(todo){
   
-  todoMap = {
-    "todo": todoText,
-    "completed": todoItem.classList.contains("completed"),
-  }
-  localTodos.push(todoMap);
-  console.log(todoMap);
-  localStorage.setItem("todos", JSON.stringify(localTodos));
-
-  todoItem.addEventListener('click', function(event){
-    todoItem.classList.toggle('completed');
-    
-    // update todoItem complete state from localStorage
-    // todoMap = {
-    //   "todo": todoText,
-    //   "completed": todoItem.classList.contains("completed"),
-    // }
-    // localTodos.push(todoMap);
-    // console.log(todoMap);
-    // localStorage.setItem("todos", JSON.stringify(localTodos));
-  });
-  
-  todoItem.addEventListener('contextmenu', function(event){
-    event.preventDefault();
-    todoItem.remove()
-    // TODO: remove element from localstorage 
-  });
-};
+}
