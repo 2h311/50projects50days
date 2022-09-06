95f1eb1d3315c5414070218c435f458ef3ffcab4
2h311 <kcodepal@outlook.com>
2022-09-06 13:54:29 +0100

    Working on making index.js update the state of the todo with event, instead of everything which appers to be easier to do

--- todo-ist/index.js
+++ todo-ist/index.js
@@ -2,15 +2,16 @@ let form = document.getElementById("todo-form");
 let formInput = document.querySelector(".form-input");
 let todoList = document.querySelector("ul.todo-list");
 
-let localTodos = localStorage.getItem("todos");
+let localTodos = JSON.parse(localStorage.getItem("todos"));
 
 if (!localTodos){
   // create a default value if it dont exist in the storage
-  localStorage.setItem("todos", JSON.stringify([])) 
+  localTodos = [];
+  localStorage.setItem("todos", JSON.stringify(localTodos)) 
 }
 
-form.addEventListener('submit', function(e){
-  e.preventDefault();
+form.addEventListener('submit', function(event){
+  event.preventDefault();
 
   // get the text the user input 
   formInputValue = formInput.value;
@@ -28,9 +29,25 @@ const addTodoItem = (todoText) =>{
   todoItem.innerHTML = todoText;
   todoList.append(todoItem);
   
+  todoMap = {
+    "todo": todoText,
+    "completed": todoItem.classList.contains("completed"),
+  }
+  localTodos.push(todoMap);
+  console.log(todoMap);
+  localStorage.setItem("todos", JSON.stringify(localTodos));
+
   todoItem.addEventListener('click', function(event){
     todoItem.classList.toggle('completed');
-    // update todoItem from localStorage
+    
+    // update todoItem complete state from localStorage
+    // todoMap = {
+    //   "todo": todoText,
+    //   "completed": todoItem.classList.contains("completed"),
+    // }
+    // localTodos.push(todoMap);
+    // console.log(todoMap);
+    // localStorage.setItem("todos", JSON.stringify(localTodos));
   });
   
   todoItem.addEventListener('contextmenu', function(event){
