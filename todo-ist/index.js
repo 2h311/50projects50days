76da4c0ab56ebc6c9770b4df6b81c7c1f2642326
2h311 <kcodepal@outlook.com>
2022-09-07 16:04:10 +0100

    I'm back to workin on the index.js, I decided to take the up again after I ran into an infinite loop yesterday

--- todo-ist/index.js
+++ todo-ist/index.js
@@ -7,9 +7,17 @@ console.log(localTodos);
 
 if (localTodos){
   // add todos to UI
-}
+  localTodos.forEach(todoElement => addTodoToUI(todoElement));
+};
 
-
-function addTodoToUI(todo){
-  
+function addTodoToUI(todoElement){
+ // create a list item to add the todo-item into
+ let todoItem = document.createElement("li");
+ todoItem.classList.add('todo-item');
+ 
+ if (todoElement.completed) {
+  todoItem.classList.add("completed");
+ }
+ todoItem.innerHTML = todoElement.text; 
+ todoList.append(todoItem);
 }
