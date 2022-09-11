84cc4cea021924bf7ec292e693b4bfc0db2d984f
2h311 <kcodepal@outlook.com>
2022-09-11 21:47:29 +0100

    index.js can now grab data from api.github.com

--- github-profiles/vanilla-gpp/index.js
+++ github-profiles/vanilla-gpp/index.js
@@ -1,44 +1,33 @@
-let searchInput = document.querySelector("#search-user");
-
+const searchInput = document.querySelector("#search-user");
 const form = document.querySelector("form");
+
+async function getUser(user){
+    const usersEndpoint = `https://api.github.com/users/${user}`;
+    try {
+        let response = await axios(usersEndpoint);
+        if(response.status === 200){
+            data = response.data;      
+            response = await axios(data.repos_url);    
+            if(response.status === 200) firstFiveRepos = response.data.slice(0, 5);
+            return {
+                "name": data.name || data.login,
+                "bio": data.bio,
+                "followers": data.followers, 
+                "followings": data.following,
+                "publicRepos": data.public_repos,
+                "avatar_url": data.avatar_url,
+                "repos": firstFiveRepos,
+            }
+        }   
+    } catch (error) {
+        return false
+    }
+};
+
 form.addEventListener("submit", function(event){
     event.preventDefault();
-
     searchedUser = searchInput.value
-    console.log(searchedUser);
-    // todo: pass searchedUser to axios 
+    getUser(searchedUser).then(data => console.log(data));
     // clear the input field
     form.reset();
 });
-
-async function getUser(){
-    const user = '2h311'
-    const usersEndpoint = `https://api.github.com/users/${user}`;
-    
-    let response = await axios(usersEndpoint);
-    
-    if(response.status === 200){
-        data = response.data;
-        console.log(data);
-        // This is a successful response
-        name = data.name;
-        bio = data.bio;
-        followers = data.followers;
-        followings = data.followings;
-        publicRepos = data.public_repos;
-        avatar = data.avatar_url;
-    }
-
-    // getREpos
-    response = await axios(usersEndpoint+"/repos");
-    if(response.status === 200){
-        data = response.data.slice(0, 5);
-        console.log(data);
-    }
-    
-    
-
-};
-
-
-getUser();
