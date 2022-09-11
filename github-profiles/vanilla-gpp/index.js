9959c1fffce417ec763f01ed69ef6472f31df74d
2h311 <kcodepal@outlook.com>
2022-09-11 14:16:48 +0100

    just got back to the github profile on day5

--- github-profiles/vanilla-gpp/index.js
+++ github-profiles/vanilla-gpp/index.js
@@ -1,1 +1,44 @@
-console.log("Hello world");
+let searchInput = document.querySelector("#search-user");
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    searchedUser = searchInput.value
+    console.log(searchedUser);
+    // todo: pass searchedUser to axios 
+    // clear the input field
+    form.reset();
+});
+
+async function getUser(){
+    const user = '2h311'
+    const usersEndpoint = `https://api.github.com/users/${user}`;
+    
+    let response = await axios(usersEndpoint);
+    
+    if(response.status === 200){
+        data = response.data;
+        console.log(data);
+        // This is a successful response
+        name = data.name;
+        bio = data.bio;
+        followers = data.followers;
+        followings = data.followings;
+        publicRepos = data.public_repos;
+        avatar = data.avatar_url;
+    }
+
+    // getREpos
+    response = await axios(usersEndpoint+"/repos");
+    if(response.status === 200){
+        data = response.data.slice(0, 5);
+        console.log(data);
+    }
+    
+    
+
+};
+
+
+getUser();
