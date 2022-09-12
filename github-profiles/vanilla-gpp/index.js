10340d3e6c72c51a32bd2248f948f8106ca473fe
2h311 <kcodepal@outlook.com>
2022-09-12 12:24:42 -0500

    styling, structure and logic is done with. just one last quirk and we done with this project

--- github-profiles/vanilla-gpp/index.js
+++ github-profiles/vanilla-gpp/index.js
@@ -27,7 +27,41 @@ async function getUser(user){
 form.addEventListener("submit", function(event){
     event.preventDefault();
     searchedUser = searchInput.value
-    getUser(searchedUser).then(data => console.log(data));
+    getUser(searchedUser).then(data => {
+        console.log(data);
+
+        const avatarImageElement = document.querySelector('img');
+        avatarImageElement.classList.add('userAvatar');
+        avatarImageElement.setAttribute("src", data.avatar_url);
+
+        document.querySelector(".user-name").innerText = data.name;
+        document.querySelector(".user-bio").innerText = data.bio;
+
+        const userRepo = document.querySelector(".user-repos");
+        data.repos.forEach((repo =>{
+            console.log(repo.name, repo.url);
+            anchorElement = document.createElement("a");
+            anchorElement.setAttribute("href", repo.html_url);
+            anchorElement.innerText = repo.name;
+            userRepo.append(anchorElement);
+        }));
+
+        // followers
+        let listElement = document.createElement("li");
+        listElement.innerHTML = `${data.followers} <strong>Followers</strong>`;
+        document.querySelector(".user-stats").appendChild(listElement);
+        console.log(data.followers);
+
+        // followings
+        listElement = document.createElement("li");
+        listElement.innerHTML = `${data.followings} <strong>Following</strong`;
+        document.querySelector(".user-stats").appendChild(listElement);
+
+        // repos
+        listElement = document.createElement("li");
+        listElement.innerHTML = `${data.publicRepos} <strong>Repo</strong`;
+        document.querySelector(".user-stats").appendChild(listElement);
+    });
     // clear the input field
     form.reset();
 });
