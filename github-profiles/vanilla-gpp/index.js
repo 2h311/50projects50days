82b6325d4e7630646a88f82e0f15fe2796894afa
2h311 <kcodepal@outlook.com>
2022-09-13 12:25:21 -0500

    Ok. Just ended copyin and pasting the original into my own work. Kinda frustrating

--- github-profiles/vanilla-gpp/index.js
+++ github-profiles/vanilla-gpp/index.js
@@ -1,81 +1,89 @@
-const searchInput = document.querySelector("#search-user");
-const form = document.querySelector("form");
-const mainElement = document.querySelector("main.user");
-const infoContainer = document.querySelector("div.info-container--inner");
+const APIURL = 'https://api.github.com/users/'
 
-async function getUser(user){
-    const usersEndpoint = `https://api.github.com/users/${user}`;
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
     try {
-        let response = await axios(usersEndpoint);
-        if(response.status === 200){
-            data = response.data;      
-            response = await axios(data.repos_url);    
-            if(response.status === 200) firstFiveRepos = response.data.slice(0, 5);
-            return {
-                "name": data.name || data.login,
-                "bio": data.bio,
-                "followers": data.followers, 
-                "followings": data.following,
-                "publicRepos": data.public_repos,
-                "avatar_url": data.avatar_url,
-                "repos": firstFiveRepos,
-            }
-        }   
-    } catch (error) {
-        return false
-    }
-};
+        const { data } = await axios(APIURL + username)
 
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-    searchedUser = searchInput.value
-    getUser(searchedUser).then(data => {
-        if (!data){
-            notify = document.querySelector(".notification");
-            notify.innerText = "No profile with this username";
-        }else{
-            const avatarImageElement = document.querySelector('img');
-            avatarImageElement.classList.add('userAvatar');
-            avatarImageElement.setAttribute("src", data.avatar_url);
-            
-            const headerElement = document.createElement("h2");
-            headerElement.classList.add("user-name");
-            headerElement.innerText = data.name;
-    
-            const paragraphElement = document.createElement("p");
-            paragraphElement.classList.add("user-bio");
-            paragraphElement.innerText = data.bio;
-    
-            const userRepo = document.createElement("div");
-            userRepo.classList.add("user-repos");
-            data.repos.forEach((repo =>{
-                anchorElement = document.createElement("a");
-                anchorElement.setAttribute("href", repo.html_url);
-                anchorElement.innerText = repo.name;
-                userRepo.append(anchorElement);
-            }));
-
-            // followers
-            let listFollowersElement = document.createElement("li");
-            listFollowersElement.innerHTML = `${data.followers} <strong>Followers</strong>`;
-            
-            // followings
-            let listFollowingElement = document.createElement("li");
-            listFollowingElement.innerHTML = `${data.followings} <strong>Following</strong`;
-            
-            // repos
-            listReposElement = document.createElement("li");
-            listReposElement.innerHTML = `${data.publicRepos} <strong>Repo</strong`;
-        
-            const unorderedListElement = document.createElement("ul");
-            unorderedListElement.classList.add("user-stats");
-            unorderedListElement.append(listFollowersElement, listFollowingElement, listReposElement);
-            
-            // clear the main.user if there's any element in it to accomodate new HTMLElements            
-            mainElement.innerHTML = "";
-            mainElement.append(headerElement, paragraphElement, unorderedListElement, userRepo); 
+        createUserCard(data)
+        getRepos(username)
+    } catch(err) {
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username')
         }
-        infoContainer.style.visibility = "visible";
-    });
-    form.reset();
-});
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
+
+        addReposToCard(data)
+    } catch(err) {
+        createErrorCard('Problem fetching repos')
+    }
+}
+
+function createUserCard(user) {
+    const cardHTML = `
+    <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${user.bio}</p>
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>
+    `
+    main.innerHTML = cardHTML
+    
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            reposEl.appendChild(repoEl)
+        })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
