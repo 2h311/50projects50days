40c84131a9e47095f9dbe3d68e824eb12e07a152
2h311 <kcodepal@outlook.com>
2022-09-17 10:25:50 -0500

    Got script.js for the quizapp working properly now. I'm on to working on the styling of the page

--- quizapp/vanilla-qzapp/script.js
+++ quizapp/vanilla-qzapp/script.js
@@ -1,3 +1,6 @@
+// TODO: Reload the questions without reloading the page 
+// TODO: Randomize the questions
+
 const quizzes = [
     {
         question: "Which language runs in a web browser?",
@@ -33,23 +36,53 @@ const quizzes = [
     },
 ]
 
+function deselectAllAnswers(){
+    questionRadios.forEach(element => element.checked = false)
+}
+
+function loadQuestion(){
+    deselectAllAnswers()
+    const currentQuiz = quizzes[quizCount]
+    questionElement.innerText = currentQuiz.question
+    questionAText.innerText = currentQuiz.a
+    questionBText.innerText = currentQuiz.b
+    questionCText.innerText = currentQuiz.c
+    questionDText.innerText = currentQuiz.d
+}
+
+function getUserAnswer(){
+    // grab the user's answer
+    user_answer = Array.from(questionRadios).find(element => element.checked == true)
+    if (user_answer){
+        // check if user answer correlates with the correct answer
+        let status = quizzes[quizCount].correct === user_answer.id
+        // if it does, score up the user
+        if(status) score++
+        // and move to the next question
+        quizCount++
+    }
+
+    if (quizCount < quizzes.length){
+        loadQuestion()
+    }else{
+        document.querySelector(".container").innerHTML = `
+        <h2>You answered ${score}/${quizzes.length} correctly</h2>
+        <button onclick="location.reload()">REload</button>
+        `
+    }
+}
+
+const questionRadios = document.querySelectorAll("input[type=radio]");
+
 const questionElement = document.getElementById("question")
-questionElement.innerText = quizzes[0].question
+const questionAText = document.getElementById("a_text")
+const questionBText = document.getElementById("b_text")
+const questionCText = document.getElementById("c_text")
+const questionDText = document.getElementById("d_text")
 
-const questionAText = document.getElementById("_a_text")
-questionAText.innerText = quizzes[0].a
+let quizCount = 0, score = 0
 
-const questionBText = document.getElementById("_b_text")
-questionBText.innerText = quizzes[0].b
-
-const questionCText = document.getElementById("_c_text")
-questionCText.innerText = quizzes[0].c
-
-const questionDText = document.getElementById("_d_text")
-questionDText.innerText = quizzes[0].d
-
+loadQuestion()
 
 const submitButton = document.querySelector("button")
-submitButton.addEventListener("click", (e)=>{
-    console.log("clciked")
-})
+submitButton.addEventListener("click", event =>  getUserAnswer())
