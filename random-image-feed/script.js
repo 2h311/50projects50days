c16541724c252f1fe5a39034f6f3dedae2c5f617
2h311 <kcodepal@outlook.com>
2022-10-13 13:30:40 -0500

    decided to add the infinite scrolling to the random-image-feed project and it worked

--- random-image-feed/script.js
+++ random-image-feed/script.js
@@ -4,8 +4,24 @@ const getRandomNr = () => Math.ceil(Math.random() * 10) + 300
 const unsplashURL = 'https://source.unsplash.com/random/'
 const container = document.querySelector('.container')
 const rows = 5
-for (let i = 0; i < rows * 3; i++){
-    const img = document.createElement('img')
-    img.src = `${unsplashURL}${getRandomSize()}`
-    container.appendChild(img)
+
+function loadImages() {
+    for (let i = 0; i < rows * 3; i++){
+        const img = document.createElement('img')
+        img.src = `${unsplashURL}${getRandomSize()}`
+        container.appendChild(img)
+    }
 }
+
+function infiniteScroll () {
+    totalPageHeight = document.body.scrollHeight
+    scrollPoint = window.scrollY + window.innerHeight
+    if (scrollPoint >= totalPageHeight) {
+        // set some timeout here so it's not instantaneous
+        // update DOM with more images
+        setTimeout(loadImages, 1000)
+    }
+}
+
+loadImages()
+window.onscroll = infiniteScroll
