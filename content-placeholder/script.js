2f2fa55d234ad869b7b46b4cba136b848a58cc3b
2h311 <kcodepal@outlook.com>
2022-10-15 14:33:15 -0500

    done with content-placeholder

--- content-placeholder/script.js
+++ content-placeholder/script.js
@@ -10,10 +10,10 @@ const animated_bg_texts = document.querySelectorAll(".animated-bg-text")
 
 setTimeout(getData, 2500)
 function getData() {
-    header.innerHTML = "<img src='' alt='' />"
+    header.innerHTML = "<img src='https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80' alt='' />"
     title.innerHTML = "Lorem ipsum dolor sit amet"
     excerpt.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis"
-    profile_img.innerHTML = ""
+    profile_img.innerHTML = "<img src='https://randomuser.me/api/portraits/men/45.jpg' alt='' />"
     name_.innerHTML = "John Doe"
     date.innerHTML = "Oct 80, 2022"
 
