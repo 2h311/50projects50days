7ba5dec0a806826b6a9314f621ed5a6f234fc8c4
2h311 <kcodepal@outlook.com>
2022-10-15 11:04:25 -0500

    added a spinner

--- random-image-feed/script.js
+++ random-image-feed/script.js
@@ -2,8 +2,13 @@ const getRandomSize = () => `${getRandomNr()}x${getRandomNr()}`
 const getRandomNr = () => Math.ceil(Math.random() * 10) + 300
 
 const unsplashURL = 'https://source.unsplash.com/random/'
-const container = document.querySelector('.container')
 const rows = 5
+
+const container = document.querySelector('.container')
+const spinnerDiv = document.querySelector(".spinner_div") 
+// create our spinner
+const spinner = document.createElement('div')
+spinner.classList.add("ids-hourglass")
 
 function loadImages() {
     for (let i = 0; i < rows * 3; i++){
@@ -17,9 +22,13 @@ function infiniteScroll () {
     totalPageHeight = document.body.scrollHeight
     scrollPoint = window.scrollY + window.innerHeight
     if (scrollPoint >= totalPageHeight) {
+        spinnerDiv.appendChild(spinner)
         // set some timeout here so it's not instantaneous
         // update DOM with more images
-        setTimeout(loadImages, 1000)
+        setTimeout(() => {
+            loadImages()
+            spinner.remove()
+        }, 1500)
     }
 }
 
