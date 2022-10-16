170d92e5e0943f2bac81af76a695f29d8f8d4f60
2h311 <kcodepal@outlook.com>
2022-10-15 20:08:40 -0500

    done with sticky-navbar @02:08

--- sticky-navbar/script.js
+++ sticky-navbar/script.js
@@ -8,3 +8,12 @@ function fixNav(){
         nav.classList.remove("active")
     }
 }
+
+// this only works in this scenario
+// if the page loads. it's a goner
+const navLinks = document.querySelector("ul.nav-links")
+const navLinkList = document.querySelectorAll("a.nav-link")
+navLinks.addEventListener("click", function(event){
+    navLinkList.forEach(link => link.classList.remove("current"))
+    event.target.classList.add("current")
+})
